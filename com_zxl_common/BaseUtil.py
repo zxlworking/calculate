a9febd9863efbb28e9668f1be0876e8fee8cd3f3
zxlworking <szzhangxiaolong@163.com>
2018-06-06 18:37:06 +0800

    create project

---
+++ com_zxl_common/BaseUtil.py
@@ -0,0 +1,7 @@
+#!/usr/bin/evn python
+#coding=utf-8
+from com_zxl_common.PrintUtil import *
+class BaseUtil:
+	mPrintUtil = PrintUtil()
+	# def __init__(self):
+	# 	self.mPrintUtil.show("BaseUtil init...")
